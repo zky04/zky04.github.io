29827e309b4de2887cc164eec89e010cb6d7a453
KY <baaaoa@outlook.com>
2025-08-07 19:04:19 +0800

    remake my pages

--- _pages/about.md
+++ _pages/about.md
@@ -1,16 +1,13 @@
 ---
-permalink: /
+layout: splash
 title: "Kunyu Zhang - Homepage"
 author_profile: true
-redirect_from: 
-  - /about/
-  - /about.html
+permalink: /
 ---
 
 # News
 - **[2025.8]** [Clinical Expert Uncertainty Guided Generalized Label Smoothing for Medical Noisy Label Learning](https://arxiv.org/pdf/2508.02495) has been submitted to arXiv!
 - **[2025.6]** [MvHo-IB: Multi-View High-Order Information Bottleneck for Brain Disorder Diagnosis](http://arxiv.org/abs/2507.02847) has been accepted by MICCAI 2025!
-
 
 # About Me
 I am an aspiring researcher passionate about applying artificial intelligence to advance healthcare and medical innovation. My research interests focus on leveraging AI technologies to solve complex medical problems and improve patient outcomes.
@@ -29,10 +26,9 @@ We address label noise in medical image datasets caused by clinical expert uncer
 **Kunyu Zhang**, Qiang Li, Shujian Yu  
 We propose a novel method to capture higher-order interactions while using information bottleneck to reduce redundancy between multi-view information for brain disorder diagnosis.
 
-
 # Research Interests
 - **Medical AI**: Applying artificial intelligence to clinical diagnosis and treatment
 - **Healthcare Technology**: Developing innovative solutions for medical challenges  
 - **Machine Learning**: Exploring ML applications in biomedical research
 - **Clinical Decision Support**: Building AI systems to assist healthcare professionals
-- **Medical Image Analysis**: Using computer vision for medical imaging applications
+- **Medical Image Analysis**: Using computer vision for medical imaging applications 
--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -8,7 +8,6 @@
 var $nav = $('#site-nav');
 var $btn = $('#site-nav button');
 var $vlinks = $('#site-nav .visible-links');
-var $vlinks_persist_tail = $vlinks.children("*.persist.tail");
 var $hlinks = $('#site-nav .hidden-links');
 
 var breaks = [];
@@ -18,39 +17,33 @@ function updateNav() {
   var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
 
   // The visible list is overflowing the nav
-  if ($vlinks.width() > availableSpace) {
+  if($vlinks.width() > availableSpace) {
 
-    while ($vlinks.width() > availableSpace && $vlinks.children("*:not(.persist)").length > 0) {
-      // Record the width of the list
-      breaks.push($vlinks.width());
+    // Record the width of the list
+    breaks.push($vlinks.width());
 
-      // Move item to the hidden list
-      $vlinks.children("*:not(.persist)").last().prependTo($hlinks);
+    // Move item to the hidden list
+    $vlinks.children().last().prependTo($hlinks);
 
-      availableSpace = $btn.hasClass("hidden") ? $nav.width() : $nav.width() - $btn.width() - 30;
-
-      // Show the dropdown btn
-      $btn.removeClass("hidden");
+    // Show the dropdown btn
+    if($btn.hasClass('hidden')) {
+      $btn.removeClass('hidden');
     }
 
-    // The visible list is not overflowing
+  // The visible list is not overflowing
   } else {
 
     // There is space for another item in the nav
-    while (breaks.length > 0 && availableSpace > breaks[breaks.length - 1]) {
+    if(availableSpace > breaks[breaks.length-1]) {
+
       // Move the item to the visible list
-      if ($vlinks_persist_tail.children().length > 0) {
-        $hlinks.children().first().insertBefore($vlinks_persist_tail);
-      } else {
-        $hlinks.children().first().appendTo($vlinks);
-      }
+      $hlinks.children().first().appendTo($vlinks);
       breaks.pop();
     }
 
     // Hide the dropdown btn if hidden list is empty
-    if (breaks.length < 1) {
+    if(breaks.length < 1) {
       $btn.addClass('hidden');
-      $btn.removeClass('close');
       $hlinks.addClass('hidden');
     }
   }
@@ -58,27 +51,20 @@ function updateNav() {
   // Keep counter updated
   $btn.attr("count", breaks.length);
 
-  // update masthead height and the body/sidebar top padding
-  var mastheadHeight = $('.masthead').height();
-  $('body').css('padding-top', mastheadHeight + 'px');
-  if ($(".author__urls-wrapper button").is(":visible")) {
-    $(".sidebar").css("padding-top", "");
-  } else {
-    $(".sidebar").css("padding-top", mastheadHeight + "px");
+  // Recur if the visible list is still overflowing the nav
+  if($vlinks.width() > availableSpace) {
+    updateNav();
   }
 
 }
 
 // Window listeners
 
-$(window).on('resize', function () {
-  updateNav();
-});
-screen.orientation.addEventListener("change", function () {
+$(window).resize(function() {
   updateNav();
 });
 
-$btn.on('click', function () {
+$btn.on('click', function() {
   $hlinks.toggleClass('hidden');
   $(this).toggleClass('close');
 });
